ec949e614a5a0e28770c8a6a3b660e74ea108989
Bilal Cherkawi <75092124+bilalcherkawi@users.noreply.github.com>
2021-04-26 18:51:07 +0300

    Update 6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("{:d} str1 to str2 School!")
+st3 = str1 +" " str2
+print("{:d} Welcome to {}".format(st3))
