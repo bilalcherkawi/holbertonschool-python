4ed897ac0d9294d63e981ac05cf263ddfde06192
bilalcherkawi <cherkawibilal7@gmail.com>
2021-04-27 14:43:20 +0000

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        flag = 0
+        if ord(x) >= ord("a") and ord(x) <= ord("z"):
+            flag = 32
+        print('{:c}'.format(ord(x) - flag), end="")
+    print()
+   
