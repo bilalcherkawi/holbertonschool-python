aabf57f4288b8fe139b40492d536556a78e0165f
Bilal Cherkawi <75092124+bilalcherkawi@users.noreply.github.com>
2021-04-27 15:47:48 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-for i in range(ord("a"), ord("z") + 1)
-    if i == ord(e) or i == ord(q):
-        continue
-    print("{}".format(i), end="")
+#!/usr/bin/python3
+for x in range(97, 123):
+    if chr(x) != "q" and chr(x) != "e":
+        print('{}'.format(chr(x)), end="")
